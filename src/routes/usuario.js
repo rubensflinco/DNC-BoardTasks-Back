ce07c382ec544751df8b2c40b10c2609ca376645
Rubens Flinco <contato@rubensflinco.com.br>
2023-03-14 20:48:04 -0300

    Nova rota de login do usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,7 @@ const express = require('express');
 const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 const conectarBancoDados = require('../middlewares/conectarBD');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const EsquemaUsuario = require('../models/usuario');
 const router = express.Router();
 
@@ -27,5 +28,39 @@ router.post('/criar', conectarBancoDados, async function(req, res) {
     return tratarErrosEsperados(res, error);
   }
 });
+
+
+
+router.post('/logar', conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { email, senha } = req.body;
+
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select('+senha');
+    if (respostaBD) {
+
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        
+        let token = jwt.sign({ id: respostaBD._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+        res.header('x-auth-token', token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso.",
+          resposta: { "x-auth-token": token }
+        });
+      } else {
+        throw new Error("Email ou senha incorreta");
+      }
+    } else {
+      throw new Error("Email ou senha incorreta");
+    }
+  } catch (err) {
+    return tratarErrosEsperados(res, err);
+  }
+});
+
+
 
 module.exports = router;
