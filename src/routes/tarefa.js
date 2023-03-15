3b5f02f144f09401c54f6864b517f5b3b0fbd9e4
Rubens Flinco <contato@rubensflinco.com.br>
2023-03-15 20:19:32 -0300

    Criação da rota de obter tarefas do usuario

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -53,4 +53,22 @@ router.put('/editar/:id', authUser, conectarBancoDados, async function(req, res)
 });
 
 
+router.get('/obter/usuario', authUser, conectarBancoDados, async function(req, res) {
+  try {
+    // #swagger.tags = ['Tarefa']
+    // #swagger.description = "Endpoint para obter todas tarefas do usuario logado."
+    const usuarioLogado = req.usuarioJwt.id;
+    const respostaBD = await EsquemaTarefa.find({usuarioCriador: usuarioLogado}).populate('usuarioCriador');
+
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Tarefas listadas na respota com sucesso.",
+      resposta: respostaBD
+    })
+
+  } catch (error) {
+    return tratarErrosEsperados(res, error);
+  }
+});
+
 module.exports = router;
