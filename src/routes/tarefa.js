e3153e6743c037e3be53e2678e35411d934b588c
Rubens Flinco <contato@rubensflinco.com.br>
2023-03-15 20:32:06 -0300

    Criação  da rota de deletar tarefa

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -6,12 +6,12 @@ const EsquemaTarefa = require('../models/tarefa');
 const router = express.Router();
 
 
-router.post('/criar', authUser, conectarBancoDados, async function(req, res) {
+router.post('/criar', authUser, conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Tarefa']
-    let {posicao, titulo, descricao, status, dataEntrega} = req.body;
+    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
     const usuarioCriador = req.usuarioJwt.id;
-    const respostaBD = await EsquemaTarefa.create({posicao, titulo, descricao, status, dataEntrega, usuarioCriador});
+    const respostaBD = await EsquemaTarefa.create({ posicao, titulo, descricao, status, dataEntrega, usuarioCriador });
 
     res.status(200).json({
       status: "OK",
@@ -25,21 +25,21 @@ router.post('/criar', authUser, conectarBancoDados, async function(req, res) {
 });
 
 
-router.put('/editar/:id', authUser, conectarBancoDados, async function(req, res) {
+router.put('/editar/:id', authUser, conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Tarefa']
     let idTarefa = req.params.id;
-    let {posicao, titulo, descricao, status, dataEntrega} = req.body;
+    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
     const usuarioLogado = req.usuarioJwt.id;
-    
+
     const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
     if (!checkTarefa) {
       throw new Error("Tarefa não encontrada ou pertence a outro usuário");
     }
 
-    const tarefaAtualizada = await EsquemaTarefa.updateOne({_id: idTarefa}, {posicao, titulo, descricao, status, dataEntrega});
-    if(tarefaAtualizada?.modifiedCount > 0){
-      const dadosTarefa = await EsquemaTarefa.findOne({_id: idTarefa}).populate('usuarioCriador');
+    const tarefaAtualizada = await EsquemaTarefa.updateOne({ _id: idTarefa }, { posicao, titulo, descricao, status, dataEntrega });
+    if (tarefaAtualizada?.modifiedCount > 0) {
+      const dadosTarefa = await EsquemaTarefa.findOne({ _id: idTarefa }).populate('usuarioCriador');
 
       res.status(200).json({
         status: "OK",
@@ -53,16 +53,40 @@ router.put('/editar/:id', authUser, conectarBancoDados, async function(req, res)
 });
 
 
-router.get('/obter/usuario', authUser, conectarBancoDados, async function(req, res) {
+router.get('/obter/usuario', authUser, conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Tarefa']
     // #swagger.description = "Endpoint para obter todas tarefas do usuario logado."
     const usuarioLogado = req.usuarioJwt.id;
-    const respostaBD = await EsquemaTarefa.find({usuarioCriador: usuarioLogado}).populate('usuarioCriador');
+    const respostaBD = await EsquemaTarefa.find({ usuarioCriador: usuarioLogado }).populate('usuarioCriador');
 
     res.status(200).json({
       status: "OK",
       statusMensagem: "Tarefas listadas na respota com sucesso.",
+      resposta: respostaBD
+    })
+
+  } catch (error) {
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+
+router.delete('/deletar/:id', authUser, conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Tarefa']
+    const idTarefa = req.params.id;
+    const usuarioLogado = req.usuarioJwt.id;
+
+    const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
+    if (!checkTarefa) {
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+    }
+
+    const respostaBD = await EsquemaTarefa.deleteOne({ _id: idTarefa });
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Tarefa deletada com sucesso.",
       resposta: respostaBD
     })
 
