73f54d832dae6e31607bf62c4afacf13016ad5e7
Rubens Flinco <contato@rubensflinco.com.br>
2023-03-15 19:30:28 -0300

    Edição de tarefa

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -25,4 +25,32 @@ router.post('/criar', authUser, conectarBancoDados, async function(req, res) {
 });
 
 
+router.put('/editar/:id', authUser, conectarBancoDados, async function(req, res) {
+  try {
+    // #swagger.tags = ['Tarefa']
+    let idTarefa = req.params.id;
+    let {posicao, titulo, descricao, status, dataEntrega} = req.body;
+    const usuarioLogado = req.usuarioJwt.id;
+    
+    const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
+    if (!checkTarefa) {
+      throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+    }
+
+    const tarefaAtualizada = await EsquemaTarefa.updateOne({_id: idTarefa}, {posicao, titulo, descricao, status, dataEntrega});
+    if(tarefaAtualizada?.modifiedCount > 0){
+      const dadosTarefa = await EsquemaTarefa.findOne({_id: idTarefa}).populate('usuarioCriador');
+
+      res.status(200).json({
+        status: "OK",
+        statusMensagem: "Tarefa atualizada com sucesso.",
+        resposta: dadosTarefa
+      })
+    }
+  } catch (error) {
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+
 module.exports = router;
