2a13772060b9586d2d3cec36b3f9b850b32ca836
Rubens Flinco <contato@rubensflinco.com.br>
2023-04-07 10:18:31 -0300

    Criação do sistema de comentarios, rota GET e POST

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 function routes(app) {
     app.use('/usuario', require('./routes/usuario.js'));
     app.use('/tarefa', require('./routes/tarefa.js'));
+    app.use('/comentario', require('./routes/comentario.js'));
     return;
 }
 
